7d5bdfbf8cb3ab712a4726ce513752a2683394ef
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-07-09 04:09:02 +0700

    Adding Controllers, Models, and Routes :sparkles:

---
+++ server/controllers/posts.js
@@ -0,0 +1,7 @@
+export const getPosts = (req, res) => {
+  res.send('This Works!');
+};
+
+export const createPost = (req, res) => {
+  res.send('Post Creation');
+};
--- server/index.js
+++ server/index.js
@@ -3,8 +3,23 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.ppoad.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
