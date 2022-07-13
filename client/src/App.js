267d89e899009ee3a03e0801e062870fb1032846
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-07-13 10:06:10 +0700

    Adding client folder :sparkles:

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memories from './images/memories.png';
+
+const App = () => {
+  return (
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+    </Container>
+  );
+};
+
+export default App;
