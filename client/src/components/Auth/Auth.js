8768add6f8836fb81f5474a0ab79bb9976ddb79e
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-02 10:33:15 +0700

    Adding Authentication components :sparkles:

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import useStyles from './styles';
+import Input from './Input';
+
+const Auth = () => {
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isSignUp = true;
+
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  half
+                />
+                <Input
+                  name="firstName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              type="email"
+              handleChange={handleChange}
+            />
+            <Input
+              name="password"
+              label="Password"
+              type={showPassword ? 'text' : 'password'}
+              handleChange={handleChange}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignUp ? 'Sign Up' : 'Sign In'}
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -22,25 +22,20 @@ const Navbar = () => {
         >
           Memories
         </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="memories"
-          height="60"
-        />
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
             <Avatar
               className={classes.purple}
-              alt={user.result.name}
-              src={user.result.imageUrl}
+              alt={user?.result.name}
+              src={user?.result.imageUrl}
             >
-              {user.result.name.charAt(0)}
+              {user?.result.name.charAt(0)}
             </Avatar>
             <Typography className={classes.userName} variant="h6">
-              {user.result.name}
+              {user?.result.name}
             </Typography>
             <Button
               variant="contained"
@@ -52,7 +47,7 @@ const Navbar = () => {
           </div>
         ) : (
           <Button
-            componoent={Link}
+            component={Link}
             to="/auth"
             variant="contained"
             color="primary"
