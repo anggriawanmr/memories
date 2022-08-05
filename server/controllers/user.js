55375ee5efb950b5059241d2a026ce49f307faed
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-05 13:40:06 +0700

    Editing user controller backend :sparkles:

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoketn';
 import { reset } from 'nodemon';
 
-import user from '../models/user';
+import User from '../models/user';
 
 export const signin = async (req, res) => {
   const { email, password } = req.body;
@@ -33,4 +33,32 @@ export const signin = async (req, res) => {
   }
 };
 
-export const signup = async (req, res) => {};
+export const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res.status(400).json({ message: 'User already exist.' });
+
+    if (password === !confirmPassword)
+      return res.status(400).json({ message: "Password don't match." });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign({ email: result.email, id: result._id }, 'test', {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
