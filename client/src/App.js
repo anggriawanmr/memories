571c4d357e69a0057a95da9e13d8c8db251d28b7
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-09 10:58:30 +0700

    Fixing Pagination :hammer:

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,11 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   return (
     <BrowserRouter>
       <Container maxwidth="xl">
@@ -16,7 +19,10 @@ const App = () => {
           <Route path="/posts" exact component={Home} />
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/:id" component={PostDetails} />
-          <Route path="/auth" component={Auth} />
+          <Route
+            path="/auth"
+            component={() => (!user ? <Auth /> : <Redirect to="/posts/" />)}
+          />
         </Switch>
       </Container>
     </BrowserRouter>
