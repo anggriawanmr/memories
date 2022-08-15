75be9459b44a2d83a06e316ec993f18a4de37864
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-15 11:43:35 +0700

    Adding PostDetails styles :sparkles:

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  Paper,
+  Typography,
+  CircularProgress,
+  Divider,
+} from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
+import { useParams, useHistory } from 'react-router-dom';
+
+import useStyles from './styles';
 
 const PostDetails = () => {
   console.log('POST DETAILS');
