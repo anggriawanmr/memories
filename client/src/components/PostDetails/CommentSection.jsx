d80fffa0e93268972d1be99cc5fecb2e278c0dd2
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-18 15:51:19 +0700

    Adding Comment Section :sparkles:

---
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef } from 'react';
+import { Typography, TextField, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import useStyles from './styles';
+
+const CommentSection = () => {
+  console.log('Comment Section');
+
+  return <h1>Comment Section</h1>;
+};
+
+export default CommentSection;
